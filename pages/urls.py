43ed0e9a8af22466012c40ef77dc02777aa4c659
Moeen_10 <108186687+The-RushCoder@users.noreply.github.com>
2025-04-23 10:47:38 +0600

    Frontend pages

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+ 
+urlpatterns = [
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+] 
---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def about_view(request):
+    return render(request, 'pages/about.html')
+
+def contact_view(request):
+    return render(request, 'pages/contact.html')
