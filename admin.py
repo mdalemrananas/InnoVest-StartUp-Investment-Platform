1018535871a214e59dbe17fd2acd44a52e2d1bde
Md. Ammar Hossain <140334526+gitForAmmar11@users.noreply.github.com>
2025-05-31 23:34:28 +0600

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import Project, ProgressUpdate
-
-admin.site.register(Project)
-admin.site.register(ProgressUpdate)
-
