4260d2d1a0aeea4c1f8ecde49ae972257b085466
Moeen <108186687+The-RushCoder@users.noreply.github.com>
2025-06-13 17:41:00 +0600

    Modified App file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+//Home and Layout
+import Layout from './components/shared/Layout';
+import Home from './components/Home';
+//Authentication
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import VerifyEmail from './components/auth/VerifyEmail';
 import ForgotPassword from './components/auth/ForgotPassword';
 import ResetPassword from './components/auth/ResetPassword';
-import Layout from './components/shared/Layout';
+//Companies
+import BrowseCompanies from './components/company/BrowseCompanies';
+import CompanyDetails from './components/company/CompanyDetails';
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route path="/browse-companies" element={<BrowseCompanies />} />
       </Routes>
     </Layout>
   );
