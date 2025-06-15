a8de5997daeeede0dc04f2debaaa9ad88d73434c
Moeen <108186687+The-RushCoder@users.noreply.github.com>
2025-06-15 15:45:42 +0600

    companies urls.py

---
+++ companies/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyViewSet, CompanyListView, FileUploadView
+
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet, basename='company')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('companies/', CompanyListView.as_view(), name='company-list'),
+    path('companies/<int:pk>/total_payments/', CompanyViewSet.as_view({'get': 'total_payments'}), name='company-total-payments'),
+    path('companies/<int:pk>/user_payments/', CompanyViewSet.as_view({'get': 'user_payments'}), name='company-user-payments'),
+    # File upload endpoint
+    path('upload-image/', FileUploadView.as_view(), name='upload-image'),
+] 
